ed68286b10e91708ecf970a201afa3a66930180d
koechkevin <koechkevin92@gmail.com>
2019-06-26 10:43:38 +0300

    allow user input page on pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { SubCountiesModule} from './sub-counties/sub-counties.module';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { WhatsappComponent } from './whatsapp/whatsapp.component';
 
-import { DataStorageService } from '../services/dataStorage.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WhatsappComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    SubCountiesModule,
     AppRoutingModule
   ],
-  providers: [DataStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
